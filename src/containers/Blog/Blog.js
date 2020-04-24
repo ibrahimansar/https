b9bd958809bf8b08f97e58909a6464c27ddb9476
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-04-24 23:53:46 +0530

    router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import axios from '../../axios'
+//import axios from '../../axios';
+import Posts from './Posts/Posts';
+import {Route} from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    state = {
-        posts :[],
-        selectedPostId: null,
-        error : false
-    }
-    componentDidMount(){
-        axios.get('/posts')
-        .then(response=> {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Ansari'
-                }
-            });
-           this.setState({posts : updatedPosts});
-        })
-        .catch(error =>{
-            //console.log(error);
-            this.setState({error: true})
-        })
-    }
-    
-    postSelectedHandler = (id) => {
-             this.setState({selectedPostId: id});
-    }
     render () {
-        let posts = <p style={{textAlign: "center"}}>something went wrong!</p>
-        if(!this.state.error){
-            posts = this.state.posts.map(post => {
-            return  <Post key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={()=> this.postSelectedHandler(post.id)}  />;
-
-        });
-
-    }
 
         return (
             <div className="Blog">
@@ -52,9 +18,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                   {posts}
-                </section>
+                {/* <Route path="/" exact render={()=> <h1>home</h1>} />
+                <Route path="/new-post" exact  render={()=> <h1>home 2</h1>} /> */}
+                <Route path= "/" exact component={Posts} />
+                <Route path= "/new-post" component={NewPost} />
             </div>
         );
     }
